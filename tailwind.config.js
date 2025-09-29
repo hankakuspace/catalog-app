e250d91c4880a1244a61cbc18f39ae8f6c10ef05
堀江雄一 <>
2025-09-29 22:11:48 +0900

    fix: tailwind content 対象を src 配下全体に拡張

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",  // src 以下をすべて対象にする
   ],
   theme: {
     extend: {},
