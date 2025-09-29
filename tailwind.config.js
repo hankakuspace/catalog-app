e40ce8d550c0cc3b93b1f810460abf02591de40f
堀江雄一 <>
2025-09-29 23:04:16 +0900

    chore: remove custom PostCSS config (Tailwind v4 auto-detect)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  safelist: [
-    "bg-black",
-    "text-white",
-    "grid-cols-4",
-    "min-h-screen",
-    "flex",
-    "flex-col",
-  ],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
 };
