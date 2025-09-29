df848f7b61e4b135d9a066455e417a8e6a23dca5
堀江雄一 <>
2025-09-29 22:56:04 +0900

    fix: Tailwind safelist を追加して bg-black/text-white/grid-cols-4 を強制生成

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    "bg-black",
+    "text-white",
+    "grid-cols-4",
+    "min-h-screen",
+    "flex",
+    "flex-col",
+  ],
   theme: {
     extend: {},
   },
