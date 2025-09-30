9185c215b7f6e701ea3a7e01121c3fd27747a5a4
堀江雄一 <>
2025-09-30 18:13:25 +0900

    chore: テスト用ファイル削除とスタイル・型関連ファイルの更新

--- src/pages/test.tsx
+++
@@ -1,8 +0,0 @@
-// src/pages/test.tsx
-export default function Test() {
-  return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Tailwind Test</h1>
-    </div>
-  );
-}
---
+++ src/types/react-quill.d.ts
@@ -0,0 +1,6 @@
+// src/types/react-quill.d.ts
+declare module "react-quill" {
+  import * as React from "react";
+  const ReactQuill: any;
+  export default ReactQuill;
+}
