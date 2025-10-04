d8f43c4e680a4dd89152dd3a178851e9a4b83194
堀江雄一 <>
2025-10-04 20:37:54 +0900

    fix: Polaris TextFieldにlabelを追加して型エラーを解消

--- src/pages/admin/catalogs/new.tsx
+++ src/pages/admin/catalogs/new.tsx
@@ -270,33 +270,36 @@ export default function NewCatalogPage() {
               />
             </BlockStack>
 
-            {/* ログイン認証 */}
-            <BlockStack gap="200">
-              <Text as="h2" variant="headingSm">
-                ログイン認証
-              </Text>
-              <InlineStack gap="200" blockAlign="center">
-                <div style={{ flex: 1 }}>
-                  <TextField
-                    labelHidden
-                    placeholder="ユーザー名"
-                    value={username}
-                    onChange={setUsername}
-                    autoComplete="off"
-                  />
-                </div>
-                <div style={{ flex: 1 }}>
-                  <TextField
-                    labelHidden
-                    placeholder="パスワード"
-                    type="password"
-                    value={password}
-                    onChange={setPassword}
-                    autoComplete="off"
-                  />
-                </div>
-              </InlineStack>
-            </BlockStack>
+         {/* ログイン認証 */}
+<BlockStack gap="200">
+  <Text as="h2" variant="headingSm">
+    ログイン認証
+  </Text>
+  <InlineStack gap="200" blockAlign="center">
+    <div style={{ flex: 1 }}>
+      <TextField
+        label="ユーザー名"
+        labelHidden
+        placeholder="ユーザー名"
+        value={username}
+        onChange={setUsername}
+        autoComplete="off"
+      />
+    </div>
+    <div style={{ flex: 1 }}>
+      <TextField
+        label="パスワード"
+        labelHidden
+        placeholder="パスワード"
+        type="password"
+        value={password}
+        onChange={setPassword}
+        autoComplete="off"
+      />
+    </div>
+  </InlineStack>
+</BlockStack>
+
 
             {/* 有効期限 */}
             <Popover
